bababa0baa78f1ce0b7309398e1b3465847fec24
Shubham Kumar <kshubham0315@gmail.com>
2025-09-01 20:21:58 +0530

    Update transcript.py

--- utils/transcript.py
+++ utils/transcript.py
@@ -1,15 +1,17 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def get_transcript(video_url):
+    # Video ID extract
     video_id = None
-    if "v=" in video_url:  # Normal YouTube link
+    if "v=" in video_url:
         video_id = video_url.split("v=")[1].split("&")[0]
-    elif "youtu.be/" in video_url:  # Short YouTube link
+    elif "youtu.be/" in video_url:
         video_id = video_url.split("youtu.be/")[1].split("?")[0]
-    
+
     if not video_id:
         raise ValueError("Invalid YouTube URL!")
 
+    # Transcript fetch
     transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
     text = " ".join([t['text'] for t in transcript])
     return text
